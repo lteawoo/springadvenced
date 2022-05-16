15a0d46f4bb5fb0de6bbd8426efb496f54741abe
lteawoo <lteawoo@naver.com>
2022-05-16 20:43:42 +0900

    전략 패턴 2번째 파라미터방식

---
+++ src/test/java/kr/taeu/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,21 @@
+package kr.taeu.advanced.trace.strategy;
+
+import kr.taeu.advanced.trace.strategy.code.ContextV2;
+import kr.taeu.advanced.trace.strategy.code.StrategyLogic1;
+import kr.taeu.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+}
