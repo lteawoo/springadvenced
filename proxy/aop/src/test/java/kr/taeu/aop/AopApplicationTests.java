825d66780db56e56c5a68691834a417f7b34bae7
lteawoo <lteawoo@naver.com>
2022-06-25 15:42:47 +0900

    modify directory structure

---
+++ proxy/aop/src/test/java/kr/taeu/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.taeu.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
