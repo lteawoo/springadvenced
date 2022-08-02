ea5cfa87300c96f895485ce693187ad0c4995889
lteawoo <lteawoo@naver.com>
2022-08-02 21:09:36 +0900

    3. 구조변경

---
+++ proxy/aop/src/main/java/kr/taeu/aop/internalcall/InternalService.java
@@ -0,0 +1,12 @@
+package kr.taeu.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class InternalService {
+    public void internal() {
+        log.info("call internal");
+    }
+}
